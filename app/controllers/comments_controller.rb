4319d2a8dc55a505744a45acfddc9e1ddfca1f0e
Dusan Orlovic <dule@trk.in.rs>
2024-12-02 01:35:45 +0100

    Add broadcasts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find params[:post_id]
+    @post.comments.create! params.expect(comment: [:content])
+    redirect_to @post
+  end
 end
